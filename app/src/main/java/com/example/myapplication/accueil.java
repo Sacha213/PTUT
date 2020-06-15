f6d8e14f59c9510e7f7b04a9aab39950af9b9f94
Sacha MONTEL <sacha.montel@univ-lyon1.fr>
2020-06-15 17:19:25 +0200

    retour à une version qui marche : page de connexion + accueil avec menu (pas en fonction)

--- app/src/main/java/com/example/myapplication/accueil.java
+++ app/src/main/java/com/example/myapplication/accueil.java
@@ -3,7 +3,6 @@ package com.example.myapplication;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,11 +10,7 @@ import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 
 
@@ -57,53 +52,13 @@ public class accueil extends AppCompatActivity {
         image.setBackgroundResource(R.drawable.article);
         layout.addView(image);
 
-        /******************* Affichage des articles *******************/
-
-        AccesBD test = new AccesBD();
-        test.execute();
 
 
 
 
         }
 
-    private class AccesBD extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try{
-
-                DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-                Class.forName("oracle.jdbc.driver.OracleDriver");
-              //  Class.forName("oracle.jdbc.driver.")
-
-                System.out.println("co");
-
-            } catch (ClassNotFoundException e) {
-
-                System.out.println("Where is your Oracle JDBC Driver?");
-
-            }
 
 
-            Connection connexionBd;
-            Statement stmt = null;
-            try {
-                connexionBd = DriverManager.getConnection("jdbc:oracle:thin:@iutdoua-oracle.univ-lyon1.fr:1521:orcl", "p1915095", "461541");
-
-                stmt = connexionBd.createStatement();
-                ResultSet resultat = stmt.executeQuery("select * from ANNONCES where IDANNONCE = 1 ");
-
-                while(resultat.next()) {
-                    System.out.println("ID : " + resultat.getInt(1) + " Nom : " + resultat.getString(2) + "Comtenu : " + resultat.getString(3));
-
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-
-            return null;
-        }
-    }
 
 }
